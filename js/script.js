b77b30de56102d6bc3416d039318e8822cb3d090
Prof1 <Lams62ua@gmail.com>
2022-10-17 09:42:11 +0300

    2022_10_16_WIN Udemy_2 041_Timer

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 
 window.addEventListener('DOMContentLoaded', () => {
 
+    //TABS...
+    //vars
     const tabs = document.querySelectorAll('.tabheader__item'), 
           tabsContent = document.querySelectorAll('.tabcontent'), 
           tabsParent = document.querySelector('.tabheader__items');
@@ -38,7 +40,65 @@ window.addEventListener('DOMContentLoaded', () => {
             }); 
         }
     });
+    //...TABS
+
+    // TIMER...
+    //Var`s
+    const deadline = '2022-10-18'; 
+
+    // Функція - returns - залишок (різницю) часу. 
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()), 
+              days = Math.floor(t / (1000 * 60 * 60 *24)), 
+              hours = Math.floor((t / (1000 * 60 * 60) % 24)), 
+              minutes = Math.floor((t / 1000 / 60) % 60), 
+              seconds = Math.floor((t / 1000) % 60);
+
+        return {
+            'total': t,
+            'days': days, 
+            'hours': hours, 
+            'minutes': minutes, 
+            'seconds': seconds,
+        }; 
+    }
+
+    // Функція - додає нуль
+    function getZero(num) {
+        if (num >= 0 && num <= 9) {
+            return `0${num}`; 
+        } else {
+            return num; 
+        }
+    }
+
+    // Функція - перенесення на сторінку
+    function setClockToPage(selector, endtime) {
+        const timer = document.querySelector(selector), 
+              days = timer.querySelector('#days'), 
+              hours = timer.querySelector('#hours'), 
+              minutes = timer.querySelector('#minutes'), 
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000); 
+        
+        // // Функція - оновлює таймер кожні 1000 мс
+        updateClock(); 
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);    // (endtime) - (deadline)
+
+            days.innerHTML = getZero(t.days); 
+            hours.innerHTML = getZero(t.hours); 
+            minutes.innerHTML = getZero(t.minutes); 
+            seconds.innerHTML = getZero(t.seconds); 
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval); 
+            }
+        }
+    }
+    setClockToPage('.timer', deadline);   // Argumenst goes to str.88
 
 
+}); // DOMContentLoaded 
 
-}); // DOMContentLoaded
